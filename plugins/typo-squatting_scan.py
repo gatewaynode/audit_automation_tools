2084c6f13f2cb558e1fbce8a628111a6aa0e079f
roadelou <roadelou@raspberrypi>
2019-07-26 23:23:37 +0100

    Started correcting the typo-squatting plugin to make it more robust.

--- plugins/typo-squatting_scan.py
+++ plugins/typo-squatting_scan.py
@@ -2,6 +2,7 @@ import jellyfish  # Used to compute distance between two strings in terms of del
 import traceback
 import requests
 import logging
+import json
 import sys
 import os
 
@@ -16,8 +17,45 @@ class Typo_Squatting_Protection(IPlugin):
     def scan(
         self, scan_list=[], output_dir="", verbose=False, debug=False, output_json=False
     ):
-        if verbose and not output_json:
-            print("Looking for potential typo-squatting in the package")
+        # Defining some smaller functions to help with the execution.
+
+        def _downloading_top_5000():
+            inventory_raw = requests.get(
+                "https://hugovk.github.io/top-pypi-packages/top-pypi-packages-365-days.json"
+            )
+            with open(
+                f"{plugin_location}/top5000_list.json", "w", encoding="utf-8"
+            ) as file:
+                file.write(inventory_raw.text)
+
+        def _loading_top_5000():
+            if os.path.exists(f"{plugin_location}/top5000_list.json"):
+                with open(
+                    f"{plugin_location}/top5000_list.json", "r", encoding="utf-8"
+                ) as file:
+                    inventory_list = json.loads(
+                        file.read()
+                    )  # Reads all the file at once, can create problems
+                    inventory = list(
+                        project["project"] for project in inventory_list["rows"]
+                    )
+                    return inventory
+            else:
+                _downloading_top_5000()
+                return _loading_top_5000()  # Recursive call because I am lazy.
+
+        def _name_distance_indicator(pkg_name_1, pkg_name_2):
+            if pkg_name_1 == pkg_name_2:
+                return float(
+                    "inf"
+                )  # We don't want the scan to report that, for instance, numpy is a name very close to that of the popular package numpy...
+            return (
+                2
+                * jellyfish.damerau_levenshtein_distance(pkg_name_1, pkg_name_2)
+                / (len(pkg_name_1) + len(pkg_name_2))
+            )
+
+        scan_errors = 0
 
         threshold = 0.3
         try:
@@ -27,55 +65,25 @@ class Typo_Squatting_Protection(IPlugin):
             scan_errors += 1
 
         with open(
-            f"{output_dir}/typo_squatting_results", "w", encoding="utf-8"
+            f"{output_dir}/typo_squatting_results.txt", "w", encoding="utf-8"
         ) as scan_results:
-            for pkg_name in pkg_names:
+            for pkg_name in scan_list:
+                if verbose and not output_json:
+                    print(
+                        f"-> Running typo-squatting detection against package {pkg_name}. Output saved to {output_dir}."
+                    )
                 try:
                     suscpicion_list = []
                     suscpicion_count = 0
                     for top_pkg in top5000_list:
                         if _name_distance_indicator(pkg_name, top_pkg) < threshold:
-                            suspicious_list.append(top_pkg)
+                            suscpicion_list.append(top_pkg)
                             suscpicion_count += 1
                     scan_results.write(
-                        f"{pkg_name}- Potential typo squattings detected -{suscpicion_count}- List of potential typo squatted packages -{suscpicion_list}\n"
+                        f"{pkg_name}- Potential typo squattings detected -{suscpicion_count}- List of potentially typo squatted packages -{suscpicion_list}\n"
                     )  # The - are here to make the output easier to parse later on.
                 except Exception as e:
                     logging.error(traceback.format_exc())
                     scan_errors += 1
 
         return scan_errors
-
-    def _downloading_top_5000():
-        inventory_raw = requests.get(
-            "https://hugovk.github.io/top-pypi-packages/top-pypi-packages-365-days.json"
-        )
-        with open(
-            f"{plugin_location}/top5000_list.json", "w", encoding="utf-8"
-        ) as file:
-            file.write(inventory_raw.text)
-
-    def _loading_top_5000():
-        if os.path.exists(f"{plugin_location}/top5000_list.json"):
-            with open(
-                f"{plugin_location}/top5000_list.json", "r", encoding="utf-8"
-            ) as file:
-                inventory_list = json.loads(
-                    file.read()
-                )  # Reads all the file at once, can create problems
-                inventory = list(
-                    project["project"] for project in inventory_list["rows"]
-                )
-                return inventory
-        else:
-            _downloading_top_5000()
-            return loading_top_5000()  # Recursive call because I am lazy.
-
-    def _name_distance_indicator(pkg_name_1, pkg_name_2):
-        if pkg_name1 == pkg_name_2:
-            return
-        return (
-            2
-            * jellyfish.damerau_levenshtein_distance(pkg_name_1, pkg_name_2)
-            / (len(pkg_name_1) + len(pkg_name_2))
-        )
